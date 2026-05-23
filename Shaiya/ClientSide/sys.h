6c8dbc6bdc29db3e5b2e3d784c791875c637a4b9
bryanpatambag <bryanpatambag@outlook.com>
2026-05-24 07:35:50 +0800

    Update sys.h

--- Shaiya/ClientSide/sys.h
+++ Shaiya/ClientSide/sys.h
@@ -5,20 +5,6 @@ RenderFunc render_tga = reinterpret_cast<RenderFunc>(render_address_tga);
 using RenderFunc = void(__cdecl*)(const char*);
 extern RenderFunc render_tga;
 
-extern char loadbar_AoL[16];
-extern char loadbar_AoL_alt1[16];
-extern char loadbar_AoL_alt2[16];
-extern char loadbar_AoL_alt3[16];
-extern char loadbar_AoL_alt4[16];
-extern char loadbar_AoL_alt5[16];
-
-extern char loadbar_UoF[16];
-extern char loadbar_UoF_alt1[16];
-extern char loadbar_UoF_alt2[16];
-extern char loadbar_UoF_alt3[16];
-extern char loadbar_UoF_alt4[16];
-extern char loadbar_UoF_alt5[16];
-
 extern char feed_button[16];
 extern char hide_button[16];
 extern char balance_button[16];
@@ -32,38 +18,8 @@ extern char online_button_hover[16];
 auto icon_loadbar_AoL = "loadbar_AoL.tga";
 char loadbar_AoL[16];
 
-auto icon_loadbar_AoL_alt1 = "loadbar_AoL1.tga";
-char loadbar_AoL_alt1[16];
-
-auto icon_loadbar_AoL_alt2 = "loadbar_AoL2.tga";
-char loadbar_AoL_alt2[16];
-
-auto icon_loadbar_AoL_alt3 = "loadbar_AoL3.tga";
-char loadbar_AoL_alt3[16];
-
-auto icon_loadbar_AoL_alt4 = "loadbar_AoL4.tga";
-char loadbar_AoL_alt4[16];
-
-auto icon_loadbar_AoL_alt5 = "loadbar_AoL5.tga";
-char loadbar_AoL_alt5[16];
-
 auto icon_loadbar_UoF = "loadbar_UoF.tga";
 char loadbar_UoF[16];
-
-auto icon_loadbar_UoF_alt1 = "loadbar_UoF1.tga";
-char loadbar_UoF_alt1[16];
-
-auto icon_loadbar_UoF_alt2 = "loadbar_UoF2.tga";
-char loadbar_UoF_alt2[16];
-
-auto icon_loadbar_UoF_alt3 = "loadbar_UoF3.tga";
-char loadbar_UoF_alt3[16];
-
-auto icon_loadbar_UoF_alt4 = "loadbar_UoF4.tga";
-char loadbar_UoF_alt4[16];
-
-auto icon_loadbar_UoF_alt5 = "loadbar_UoF5.tga";
-char loadbar_UoF_alt5[16];
 
 auto icon_online_background = "online_background.tga";
 char online_background[16];
